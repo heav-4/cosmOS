5b66f90ae085ba126bbd63c44bd72e63bf69b3dd
heav-4 <46372165+heav-4@users.noreply.github.com>
2020-05-14 23:39:54 +0100

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,1 +1,29 @@
-print("Hi!")
+local function fwrite(f,i)
+  local f=fs.open(f,"w")
+  f.write(i)
+  f.close()
+end
+
+local function fread(f)
+  local f=fs.open(f,"r")
+  local i=f.readAll()
+  f.close()
+  return i
+end
+
+local function fwrite_lon(f,j)
+  local f=fs.open(f,"w")
+  f.write(textutils.serialize(j))
+  f.close()
+end
+
+local function fread_lon(f)
+  local f=fs.open(f,"r")
+  local i=textutils.unserialize(f.readAll())
+  f.close()
+  return i
+end
+
+if not fs.exists(".cosmOS_fvi.lon") then
+  fwrite_json(".cosmOS_fvi.lon",{})
+end
