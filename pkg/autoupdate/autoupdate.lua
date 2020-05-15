f7adfdc2bd439e714a0727cd9f41b2eef7846cf6
heav-4 <heavpoot@gmail.com>
2020-05-15 11:35:10 +0100

    move autoupdate into a package for future

---
+++ pkg/autoupdate/autoupdate.lua
@@ -0,0 +1,36 @@
+local s,e=pcall(function()
+  local function fwrite(f,i)
+    local f=fs.open(f,"w")
+    f.write(i)
+    f.close()
+  end
+
+  local function fread(f)
+    local f=fs.open(f,"r")
+    local i=f.readAll()
+    f.close()
+    return i
+  end
+
+  local function fwrite_lon(f,j)
+    local f=fs.open(f,"w")
+    f.write(textutils.serialize(j))
+    f.close()
+  end
+
+  local function fread_lon(f)
+    local f=fs.open(f,"r")
+    local i=textutils.unserialize(f.readAll())
+    f.close()
+    return i
+  end
+
+  if not fs.exists(".cosmOS_fvi.lon") then
+    fwrite_lon(".cosmOS_fvi.lon",{})
+  end
+end)
+if not s then
+  print("oh no, a bad thing happened: \n"..e)
+end
+
+--hi. im updating this via terminal instead of githubs bad page
